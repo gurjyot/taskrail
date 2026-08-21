5b01b04a6d6febcf374f7ab1fb1b3ab17695dc85
Gurjyot Singh <thegurjyot@gmail.com>
2026-08-21 22:08:52 +0530

    Add low-risk TaskRail CLI dispatcher

---
+++ src/taskrail-cli.ts
@@ -0,0 +1,9 @@
+const command = process.argv[2] || '--help';
+const compositionCommands = new Set(['components', 'component', 'capability-find', 'capability-check', 'init']);
+
+if (compositionCommands.has(command)) {
+  const { runCompositionCli } = await import('./composition-cli.js');
+  await runCompositionCli(process.argv.slice(2));
+} else {
+  await import('./cli.js');
+}
