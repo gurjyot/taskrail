e9540da149530926c3d1125a02614e693ab302af
Gurjyot Singh <thegurjyot@gmail.com>
2026-08-21 22:15:15 +0530

    Align public component namespaces with catalog

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export {
+import {
   createExecutionContext,
   LocalStateStore,
   IdempotencyStore,
@@ -8,6 +8,24 @@ export {
   mapConcurrent,
   effectiveExecutionPolicy,
 } from '../execution.js';
+
+export {
+  createExecutionContext,
+  LocalStateStore,
+  IdempotencyStore,
+  runIdempotent,
+  withRetry,
+  withTimeout,
+  mapConcurrent,
+  effectiveExecutionPolicy,
+};
+
+export const execution = Object.freeze({ createExecutionContext, effectiveExecutionPolicy });
+export const state = Object.freeze({ LocalStateStore });
+export const idempotency = Object.freeze({ IdempotencyStore, runIdempotent });
+export const retry = Object.freeze({ withRetry });
+export const timeout = Object.freeze({ withTimeout });
+export const concurrency = Object.freeze({ mapConcurrent });
 
 export * as config from './config.js';
 export * as fsSafe from './fs-safe.js';
